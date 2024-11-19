6a4ce4b8bb84bde95e3ff88d43dd3a688b823889
jackerMany <jackkhab242@gmail.com>
2024-11-19 11:50:40 +0800

    add tax
    please merge my code joseph

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("welcome to bsit hotel accomodatoins:");
+        // By steph
+        System.out.println("Tax = 12%");
     }
 }
